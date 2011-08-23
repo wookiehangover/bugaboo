65539f0ffb18865b40966b14ac36e8829b86a439
Nico Valencia <nico@quickleft.com>
2011-08-23 09:51:27 -0600

    adding initial rails stack

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bugaboo::Application.config.secret_token = '0ad839cb8385d791ec194c46a6eb2676d554a684dc6a1e1f668e173ac8ac74dda9af23d7110f6ef3b331bded6de246e3a0c8ae1de1dc9bf6d086aa1e71685ffe'
